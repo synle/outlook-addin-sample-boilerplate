e018ed43c1e4a20dc945fbc31f8d638fae5da1a1
Sy Le <lenguyensy@gmail.com>
2017-08-05 22:18:07 -0700

    functional manifest

--- generate_manifest.sh
+++ generate_manifest.sh
@@ -6,4 +6,6 @@ file_manifest=manifest.xml
 cp manifest.sample.xml manifest.xml
 echo "generate manifest: $file_manifest"
 perl -pi -e 's/{addin_app_uuid}/7164e750-dc86-49c0-b548-1bac57abdddd/g' $file_manifest
-perl -pi -e 's/{addin_app_url}/https:\/\/localhost.com:9001/g' $file_manifest
+perl -pi -e 's/{addin_app_url}/https:\/\/localhost.com/g' $file_manifest
+perl -pi -e 's/{addin_app_title}/Demo Addin/g' $file_manifest
+perl -pi -e 's/{addin_app_description}/Demo Addin Description/g' $file_manifest
